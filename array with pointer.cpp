83ec1926686d74fe5c890bb41f9fef82f2f18ca2
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2021-05-12 09:36:40 +0530

    Array with pointer.

---
+++ array with pointer.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int array []={78,45,12,89,56,23,79,46,13,82};  
+    int *ptr;                                                    
+
+    ptr = array;                                                   
+    cout << "\n Values : ";
+    for(int a=1;a<=10;a++)
+    {
+    	cout << *ptr;                                     
+        ptr++;                                                          
+    }
+}
+
